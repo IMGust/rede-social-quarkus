a4a65dd033c85fffdb4d9f1dae8440552abb7395
Gustavo Assis <gdwassis@gmail.com>
2025-06-30 10:37:14 -0300

    testeUnitário_Users

---
+++ src/test/java/quarkusTest/UserResourceTest.java
@@ -0,0 +1,93 @@
+package quarkusTest;
+
+import dto.CreateUserRequest;
+import dto.ResponseError;
+import io.quarkus.test.common.http.TestHTTPResource;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.*;
+
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.responseSpecification;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@QuarkusTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class UserResourceTest {
+
+    @TestHTTPResource("/users")
+    URL apiURL;
+
+
+
+    @Test
+    @DisplayName("should create an user successfully")
+    @Order(1)
+    public void createdUserTest(){
+        var user = new CreateUserRequest();
+        user.setNome("Fulano");
+        user.setIdade(10);
+
+
+        var response = given()
+                        .contentType(ContentType.JSON)
+                        .body(user).
+                when()
+                .post(apiURL).
+                then().
+                        extract().response();
+
+        assertEquals(201,response.statusCode());
+        assertNotNull(response.jsonPath().getString("id"));
+    }
+
+    @Test
+    @DisplayName("should return error when json is not valid")
+    @Order(2)
+    public void createUserValidationErrorTest(){
+
+        var user = new CreateUserRequest();
+        user.setNome(null);
+        user.setIdade(null);
+
+        var response = given()
+                .contentType(ContentType.JSON)
+                .body(user)
+                .when()
+                .post(apiURL)
+                .then()
+                .extract()
+                .response();
+            assertEquals(ResponseError.UNPROCESSABLE_ENTITY_STATUS, response.statusCode());
+            assertEquals("Validation error", response.jsonPath().getString("message"));
+
+        List<Map<String, String>> errors = response.jsonPath().getList("errors");
+        assertNotNull(errors.get(0).get("message"));
+        assertNotNull(errors.get(1).get("message"));
+
+
+    }
+    @Test
+    @DisplayName("should list all users")
+    @Order(3)
+    public void listAllUsersTest(){
+
+        var list = given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get(apiURL)
+                .then()
+                .statusCode(200)
+                .body("size()", Matchers.is(1));
+
+
+
+    }
+
+}
